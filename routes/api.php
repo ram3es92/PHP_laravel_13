df76c064b8f559db399373f378f11af5b69e5c89
Роман Князев <ram3es92@mail.ru>
2025-02-23 21:37:08 +0300

    Изменения в ходе тестирования функций продуктКонтроллера

--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,9 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::apiResource('/products', ProductController::class);
+
+Route::get('/products', [ProductController::class, 'index']);        // Получить список товаров
+Route::get('/products/{id}', [ProductController::class, 'show']);    // Получить один товар
+Route::post('/products', [ProductController::class, 'store']);       // Создать товар
+Route::patch('/products/{id}', [ProductController::class, 'update']);  // Обновить товар
+Route::delete('/products/{id}', [ProductController::class, 'destroy']); // Удалить товар
